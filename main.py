cb46cf83074fb00b36ccbf8b5b0180a1edddb783
Eric Langdon <eric.langdon@uvm.edu>
2019-10-16 14:36:47 -0400

    ignore bad lines

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv(os.getcwd() + '/data/pitchdata2019.csv')
+data = pd.read_csv(os.getcwd() + '/data/pitchData2019.csv', error_bad_lines=False)
 print (data)
